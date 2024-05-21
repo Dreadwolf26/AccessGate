3b98b571a0cd26fcb935aae34aefb9f34864384d
Chris <chrisjholcombe@gmail.com>
2024-05-21 08:34:05 -0400

    Create app.py
    Created comments as to dos as a general over view of the application

---
+++ app.py
@@ -0,0 +1,71 @@
+'''
+User Authentication API
+
+
+Learning resources: 
+
+FastAPI Documentation: FastAPI: https://fastapi.tiangolo.com/
+OAuth2 with FastAPI: OAuth2 FastAPI: https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/
+Python OAuthLib Documentation: OAuthLib: https://oauthlib.readthedocs.io/en/latest/
+JWT (JSON Web Tokens): JWT.io: https://jwt.io/
+
+'''
+
+#import necessary libraries 
+
+
+#Instantiate Flask
+
+
+#Establish database
+
+
+#create index page for API usage 
+
+
+
+#create endpoint for user registration 
+
+#get user data (Username/Password)
+
+#using hashing algo to salt and hash passwords 
+
+#commit to database
+
+
+
+
+#user login route 
+
+#validate credentials and issue a json web token JWT
+
+
+
+
+
+
+#password reset endpoit
+
+
+#password reset link or reset code via email (see nutritrack for example)
+
+
+
+
+#create ennd point to update password using link or code
+
+
+
+
+
+
+
+#User profile endpoint
+
+#fetch and update user information from the db
+
+
+
+
+
+#OAuth integration (Allow users to log in from third party google, facebook etc.)
